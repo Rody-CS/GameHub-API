d7fd0c085182aa4fe82da88b98bc9ec406cf7a0c
Rodrigo Castro <rodrigocd28@gmail.com>
2025-03-29 09:16:47 -0300

    Adiciona projeto Spring GameHub ao repositório

---
+++ src/test/java/me/dio/gamehub/GameHubApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.gamehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
