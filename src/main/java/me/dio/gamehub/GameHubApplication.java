ce5733675afd53aeb9ad78b411adf9a541b8c06c
Rodrigo Castro <rodrigocd28@gmail.com>
2025-04-06 18:13:37 -0300

    Correção CORS

--- src/main/java/me/dio/gamehub/GameHubApplication.java
+++ src/main/java/me/dio/gamehub/GameHubApplication.java
@@ -1,10 +1,13 @@
 package me.dio.gamehub;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @Configuration
 @EnableTransactionManagement
 @SpringBootApplication
